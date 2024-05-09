b3fcd620b6803da98d59a38b0a506c6e613704b4
Boby <44772249+bori0409@users.noreply.github.com>
2024-05-09 12:30:31 +0200

    works

--- ms/src/app/app.routes.ts
+++ ms/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { ContainerComponent } from '../components/container/container.component';
 
+
 export const routes: Routes = [
-    { path: 'container', component:ContainerComponent },
+   { path: '', redirectTo: 'container', pathMatch: 'full'},
+   { path: 'container', component:ContainerComponent },
 ];
+
+
+
--- ms/src/components/container/container.component.ts
+++ ms/src/components/container/container.component.ts
@@ -1,30 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
 import webgazer from 'webgazer';
 @Component({
   selector: 'app-container',
   standalone: true,
   imports: [],
   templateUrl: './container.component.html',
-  styleUrl: './container.component.scss'
+  styleUrl: './container.component.scss',
 })
-export class ContainerComponent implements OnInit{
+export class ContainerComponent implements OnInit {
   ngOnInit(): void {
     console.log('Webgazer', webgazer);
-    webgazer.setRegression('ridge').setGazeListener(function(data:any, elapsedTime:any) {
-      console.log(data); //elapsed time is based on time since begin was called
-      console.log(data);
-      var prediction = webgazer.getCurrentPrediction();
-      if (prediction) {
-        var x = prediction.x;
-        var y = prediction.y;
-        console.log(prediction)
-      }
-      
-    }).begin();
+    webgazer
+      .setRegression('ridge')
+      .setGazeListener(function (data: any, elapsedTime: any) {
+        var prediction = webgazer.getCurrentPrediction().then((pred: any) => {
+          console.log('Please:', pred);
+        });
+      })
+      .begin();
 
     webgazer.showPredictionPoints(true);
   }
- 
-
 }
